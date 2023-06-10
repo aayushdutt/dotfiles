23c06ed979fa7c301aaee88c4a6830419ff6a73c
Aayush Dutt <aayushd100@gmail.com>
2023-06-10 14:52:05 +0530

    Fix setup.py script

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ```sh
-wget -qO- https://raw.githubusercontent.com/aayushdutt/dotfiles/main/setup.py | python
+wget -qO- aayushdutt.com/setup | python3
+
+OR
+
+wget -qO- https://raw.githubusercontent.com/aayushdutt/dotfiles/main/setup.py | python3
 ```
--- setup.py
+++ setup.py
@@ -5,31 +5,50 @@ except ImportError:
     from urllib import urlretrieve
 
 
-URL_BASE="https://raw.githubusercontent.com/aayushdutt/dotfiles/main"
+URL_BASE = "https://raw.githubusercontent.com/aayushdutt/dotfiles/main"
+
+
+def create_required_dirs(filepath: str) -> bool:
+    directory, _filename = os.path.split(filepath)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+    # create file if doesn't exist
+    os.close(os.open(filepath, os.O_CREAT))
+
+    # Check if the file has a non-zero size
+    if os.path.getsize(filepath) > 0:
+        print(
+            f"Skipping download for {filepath}. File already exists and is not empty.")
+        return True
+    return False
+
 
 def download(filepath: str):
-  directory, _filename = os.path.split(filepath)
-  if directory:
-    os.makedirs(directory, exist_ok=True)
+    home_directory = os.path.expanduser('~')
+    abspath = os.path.join(home_directory, filepath)
 
-  # Check if the file already exists and has a non-zero size
-  if os.path.isfile(filepath) and os.path.getsize(filepath) > 0:
-      print(f"Skipping download for {filepath}. File already exists and is not empty.")
-      return
+    already_exists = create_required_dirs(abspath)
+    if already_exists:
+        return
+    
+    url = f'{URL_BASE}/{filepath}'
+    print(f'Downloading ~/{filepath} from {url}')
 
-  return urlretrieve(f'{URL_BASE}/{filepath}', f'~/{filepath}')
+    return urlretrieve(url, abspath)
+
 
 def setup():
-  """
-  TODO:
-  1. Parallelize downloads
-  2. Add package installations
-  3. CLI options for OS and to only setup one of packages or dotfiles
-  """
-  download('.bash_aliases')
-  download('.bash_profile')
-  download('.tmux.conf')
-  download('.config/nvim/init.lua')
-  
+    """
+    TODO:
+    1. Parallelize downloads
+    2. Add package installations
+    3. CLI options for OS and to only setup one of packages or dotfiles
+    """
+    download('.bash_aliases')
+    download('.bash_profile')
+    download('.tmux.conf')
+    download('.config/nvim/init.lua')
+
+
 if __name__ == "__main__":
-  setup()
+    setup()
